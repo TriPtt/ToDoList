2f9eed614d54ae69e20769ac9053b47782e9d96c
Ugo SH <ugosh16@gmail.com>
2024-01-03 11:55:13 +0100

    Modifie app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,24 @@ import { TasksController } from './tasks/tasks.controller';
 import { TasksService } from './tasks/tasks.service';
 import { TasksModule } from './tasks/tasks.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+    ConfigModule.forRoot(),
+    TasksModule,
+  ],
   controllers: [AppController, UsersController, TasksController],
   providers: [AppService, UsersService, TasksService],
 })
