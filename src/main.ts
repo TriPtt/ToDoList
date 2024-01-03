fc4488f907445affdfbbd448663db251f9147d0b
Tristan Petit <tristan.petit01@gmail.com>
2024-01-03 15:07:46 +0100

    Ajout dependance swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('ToDoList API')
+    .setDescription('API for ToDoList')
+    .setVersion('0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
   await app.listen(3000);
   console.log(process.env.API_PORT);
   console.log(process.env.DATABASE_USER);
