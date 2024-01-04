986a0ff2affaad39d74ee6b3d1db3a19f20f3069
lolfi33 <yoayoann@gmail.com>
2024-01-04 15:37:26 +0100

    route pour suppression, creation et listage de toutes les routes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,47 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Delete,
+  Param,
+  NotFoundException,
+  InternalServerErrorException,
+  Post,
+  Body,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Tasks } from './tasks.entity';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
   @Get()
-  return(): string {
-    return 'La route tasks fonctionne bien';
+  findAll(): Promise<Tasks[]> {
+    return this.tasksService.findAll();
+  }
+
+  @Post()
+  async create(@Body() taskData: Partial<Tasks>): Promise<Tasks> {
+    try {
+      return await this.tasksService.add(taskData);
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Une erreur interne est survenue.'
+      );
+    }
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<void> {
+    try {
+      await this.tasksService.remove(+id);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      } else {
+        throw new InternalServerErrorException(
+          'Une erreur interne est survenue.'
+        );
+      }
+    }
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Tasks } from './tasks.entity';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    @InjectRepository(Tasks)
+    private tasksRepository: Repository<Tasks>
+  ) {}
+
+  findAll(): Promise<Tasks[]> {
+    return this.tasksRepository.find();
+  }
+
+  findOne(id: number): Promise<Tasks | null> {
+    return this.tasksRepository.findOneBy({ id });
+  }
+
+  async remove(id: number): Promise<void> {
+    const task = await this.tasksRepository.findOne({ where: { id } });
+    if (!task) {
+      throw new NotFoundException(
+        `La tâche avec l'ID ${id} n'a pas été trouvée.`
+      );
+    }
+    await this.tasksRepository.remove(task);
+  }
+
+  async add(taskData: Partial<Tasks>): Promise<Tasks> {
+    const newTask = this.tasksRepository.create(taskData);
+    return await this.tasksRepository.save(newTask);
+  }
+}
