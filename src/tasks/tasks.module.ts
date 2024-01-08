8f681047838dfc4f2282ffa0cb6e8187cc3ce231
Tristan Petit <tristan.petit01@gmail.com>
2024-01-08 14:53:34 +0100

    test

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { Tasks } from './tasks.entity';
+import { Users } from 'src/users/users.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tasks])],
-  providers: [TasksService],
+  imports: [TypeOrmModule.forFeature([Tasks, Users])],
   controllers: [TasksController],
+  providers: [TasksService], // Include UsersRepository here
 })
 export class TasksModule {}
