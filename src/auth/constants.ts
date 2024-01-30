149092922908ad5f2537350bdda0bb206ef15d42
Ugo SH <ugosh16@gmail.com>
2024-01-30 14:42:09 +0100

    Modification jwt secret key

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -3,7 +3,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 const configService = new ConfigService();
 
 export const jwtConstants = {
-  secret:
-    configService.get('JWT_SECRET_KEY') ||
-    '689fbdc9b80633570f1bb1cd2c90ba71039891fc292bb81c685f3ac850b71bfb',
+  secret: configService.get('JWT_SECRET_KEY'),
+  inject: [ConfigService],
 };
